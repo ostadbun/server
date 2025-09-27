ee9da9068d3a971829613fa133926c0e7c3f63aa
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-09-27 23:02:23 +0330

    add env file for goose , add docker compose for run postgerss on docker and install fiber for http engine

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	fmt.Println("OSTADBUN is right her")
-	fmt.Println("Mammd tala")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
+
 }
