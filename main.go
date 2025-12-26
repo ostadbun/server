5630044cc9f74c657ac3068281cb3a2b0313862e
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-12-26 07:29:47 +0330

    modify main.go and make file and dependencies

--- main.go
+++ main.go
@@ -1,31 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"ostadbun/controller"
-	"ostadbun/database"
+	"github.com/joho/godotenv"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
+	"ostadbun/delivery/httpserver"
+	"ostadbun/service/oauthservice"
+	"ostadbun/service/userservice"
 )
 
 func main() {
-	app := fiber.New()
-	app.Use(cors.New())
-
-	dbConf := database.Config()
-
-	controller.Config(app, dbConf)
-
-	routes := app.Stack()
-
-	fmt.Println("Registered Routes:")
-	for _, stack := range routes {
-		for _, route := range stack {
-			fmt.Printf("  Method: %s, Path: %s\n", route.Method, route.Path)
-		}
+	err := godotenv.Load()
+	if err != nil {
+		panic("error loading .env file")
 	}
+	//dbConf := database.New()
 
-	log.Fatal(app.Listen(":3000"))
+	oauth := oauthservice.NewOAuthService()
+	userSvc := userservice.New(*oauth)
+
+	server := httpserver.New(userSvc)
+
+	server.Serve()
+
 }
--- makefile
+++ makefile
@@ -3,10 +3,10 @@ DB_URL=postgres://username:password@localhost:5432/ostadbun?sslmode=disable
 create:
 	@migrate create -ext sql -dir migrations -seq $(dis)
 up:
-	migrate -path migrations -database "$(DB_URL)" up
+	migrate -path database/migrations -database "$(DB_URL)" up
 
 down:
-	migrate -path migrations -database "$(DB_URL)" down 1
+	migrate -path database/migrations -database "$(DB_URL)" down 1
 
 run:
 	DATABASE_URL="$(DB_URL)"  go run main.go
