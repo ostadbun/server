9b15939d040687a01c098e3bed6fb9805f82d7fa
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-12-26 07:28:20 +0330

    add mock repository

--- repository/auth/auth.go
+++ repository/auth/auth.go
@@ -1,17 +1,19 @@
 package authReposipory
 
-import "github.com/jackc/pgx/v5"
+import (
+	"database/sql"
+)
 
 type AuthRepo struct {
-	db *pgx.Conn
+	db *sql.DB
 }
 
-func Make(db *pgx.Conn) AuthRepo {
+func Make(db *sql.DB) AuthRepo {
 	return AuthRepo{
 		db: db,
 	}
 }
 
-func (a AuthRepo) createUser(username string, password string) error {
+func (a AuthRepo) CreateUser(username string, password string) error {
 	panic("implement me")
 }
