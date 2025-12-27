417005722d3ee06678a8c0a1cb7005c649faa010
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-12-28 00:19:38 +0330

    add redis to dependencies

--- delivery/httpserver/userhandler/handler.go
+++ delivery/httpserver/userhandler/handler.go
@@ -2,14 +2,18 @@ package userhandler
 
 import (
 	"ostadbun/service/userservice"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type Handler struct {
 	authSvc userservice.User
+	redis   *redis.Client
 }
 
-func New(authSvc userservice.User) Handler {
+func New(authSvc userservice.User, redis *redis.Client) Handler {
 	return Handler{
 		authSvc: authSvc,
+		redis:   redis,
 	}
 }
