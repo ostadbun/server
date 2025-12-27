9978874fe085274d972c6de8b24efde1fef4750a
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2025-12-28 00:16:47 +0330

    add hasher package for sha256 hashing

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -3,8 +3,21 @@ package hash
 import (
 	"crypto/sha256"
 	"fmt"
+	"os"
 )
 
 func Hasher(text string) string {
-	return fmt.Sprintf("%x", sha256.Sum256([]byte(text)))
+
+	SecretKey1 := os.Getenv("SECRET_HASHER1")
+	SecretKey3 := os.Getenv("SECRET_HASHER3")
+	SecretKey5 := os.Getenv("SECRET_HASHER5")
+
+	txt := fmt.Sprintf("(((%s)%s)%s)%s)",
+		SecretKey3,
+		SecretKey5,
+		text,
+		SecretKey1,
+	)
+
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(txt)))
 }
