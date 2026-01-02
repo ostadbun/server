4d0c56c0ecad9c84c3111845e7b337403f51d5a6
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2026-01-03 00:49:07 +0330

    add manipulation permission middleware

---
+++ delivery/httpserver/middlewares/basicmanipulationPermission.go
@@ -0,0 +1,43 @@
+package middlewares
+
+import (
+	"os"
+	"ostadbun/pkg/httpstorage"
+	manipulation "ostadbun/service/manipulationService"
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func ManipulationPermission(m manipulation.Manipulation) func(c *fiber.Ctx) error {
+
+	return func(c *fiber.Ctx) error {
+
+		userId, Err := httpstorage.Get(c, "user_id").Number()
+
+		if Err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": Err.Error()})
+		}
+
+		level, errG := m.GetUserLevel(userId)
+
+		if errG != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(errG.Error())
+		}
+
+		scre := os.Getenv("BASICPERMISSIONSCORE")
+
+		score, err := strconv.Atoi(scre)
+
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"message": "score not a valid number"})
+		}
+
+		if level < score {
+			return c.Status(fiber.StatusForbidden).SendString("you can not manipulate")
+		}
+
+		return c.Next()
+
+	}
+}
