e9619e67706d53602024c4ae7ad3e5f9a078d1b6
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2026-02-10 21:27:38 +0330

    remove a.go for build on koyeb

--- adaptor/redisAdaptor/main.go
+++ adaptor/redisAdaptor/main.go
@@ -1,18 +1,44 @@
 package redisAdaptor
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/redis/go-redis/v9"
 )
 
-
-
 func New() *redis.Client {
+
+	dbNumber, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+
+	if err != nil {
+		dbNumber = 0
+	}
+
+	ProNumber, err := strconv.Atoi(os.Getenv("REDIS_PROTOCOL"))
+
+	if err != nil {
+		ProNumber = 2
+	}
+
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password
-		DB:       0,  // use default DB
-		Protocol: 2,
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Username: os.Getenv("REDIS_USERNAME"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       dbNumber,
+		Protocol: ProNumber,
 	})
 
 	return rdb
 }
+
+//REDIS_ADDR=localhost:6379
+//#REDIS_ADDR=redis-17273.c311.eu-central-1-1.ec2.cloud.redislabs.com:17273
+//
+//REDIS_USERNAME=
+//#REDIS_USERNAME=default
+//
+//REDIS_PASSWORD=
+//#REDIS_PASSWORD=DeCiC4nD9zCgNscpCd40SlkHRTj0WufT
+//
+//REDIS_DB=0
